d145d4e5031c205c250d3e8697defeead3003c17
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-18 16:37:07 -0400

    Update default_path.sh

--- default_path.sh
+++ default_path.sh
@@ -1,2 +1,3 @@
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/QD899836/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/QD899836/.bashrc
+source /users/QD899836/.bashrc
