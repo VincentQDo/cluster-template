862febf7bc05b82b2b5bf4fb7bb8062cacbb2bbd
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-10 13:21:55 -0500

    Update install_slurm.sh

--- install_slurm.sh
+++ install_slurm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #1.0
 sudo yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
-cd /scratch/
-#link from the tutorial from slothparadise is dead so this is a new link
-wget https://download.schedmd.com/slurm/slurm-18.08.3.tar.bz2
 #need to install this for RPM to work
 sudo yum install perl-ExtUtils-MakeMaker -y
 
