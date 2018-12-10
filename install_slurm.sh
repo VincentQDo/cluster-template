41ae39059bb1c52b82c899d0c0f0af7a6171387d
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-10 13:07:30 -0500

    Update install_slurm.sh

--- install_slurm.sh
+++ install_slurm.sh
@@ -9,5 +9,10 @@ sudo yum install perl-ExtUtils-MakeMaker -y
 
 sudo yum install rpm-build
 #make sure the slurm version match with the slurm in wget
-#this may take a while to complete 
+#this take roughly 13 minutes to complete
 sudo rpmbuild -ta slurm-18.08.3.tar.bz2
+mkdir /scratch/slurm-rpms
+#copy everything from rpmbuild to the shared folder
+sudo cp /root/rpmbuild/RPMS/x86_64/* /scratch/slurm-rpms/
+#not sure why this command does not work, it cannot open the rpm files
+yum --nogpgcheck localinstall slurm-15.08.9-1.el7.centos.x86_64.rpm slurm-devel-15.08.9-1.el7.centos.x86_64.rpm slurm-munge-15.08.9-1.el7.centos.x86_64.rpm slurm-perlapi-15.08.9-1.el7.centos.x86_64.rpm slurm-plugins-15.08.9-1.el7.centos.x86_64.rpm slurm-sjobexit-15.08.9-1.el7.centos.x86_64.rpm slurm-sjstat-15.08.9-1.el7.centos.x86_64.rpm slurm-torque-15.08.9-1.el7.centos.x86_64.rpm
