28858731853b2431d9eb71d9dd283adf199eea2f
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-10 13:27:13 -0500

    Update create_rpmbuild.sh

--- create_rpmbuild.sh
+++ create_rpmbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum install rpm-build
+sudo yum install perl-ExtUtils-MakeMaker -y
 #make sure the slurm version match with the slurm in wget
 #this take roughly 13 minutes to complete
 sudo rpmbuild -ta slurm-18.08.3.tar.bz2
