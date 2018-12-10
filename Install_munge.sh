63c00142051b6dce22b1235e18b89d49ac8688f2
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-10 11:20:15 -0500

    added munge install

--- Install_munge.sh
+++ Install_munge.sh
@@ -1,4 +1,4 @@
-
+#1.1
 #!/bin/bash 
 set -x 
 #install mariadb
@@ -11,4 +11,5 @@ export SLURMUSER=992
 sudo groupadd -g $SLURMUSER slurm
 sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
 
-sudo yum install epel-release
+sudo yum install epel-release -y
+sudo yum install munge munge-libs munge-devel -y
