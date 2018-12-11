ca981f24e1fdadb63b9e2ea197c4c0cac7ac8509
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 15:58:56 -0500

    Update install_rpms.sh

--- install_rpms.sh
+++ install_rpms.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+while [ ! -f /scratch/rpm.done ]
+do
+  sleep 10
+done
 sudo yum --nogpgcheck localinstall /software/slurm-rpms/* -y
 #to test if this worked just re run it and if you see nothing to do then it executed perfectly
