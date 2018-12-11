67147252b46f667a94876ec5485f1660422c96c5
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 15:27:00 -0500

    correct the path for the rpm build file

--- create_rpmbuild.sh
+++ create_rpmbuild.sh
@@ -2,7 +2,7 @@
 sudo yum install perl-ExtUtils-MakeMaker -y
 #make sure the slurm version match with the slurm in wget
 #this take roughly 13 minutes to complete
-sudo rpmbuild -ta slurm-18.08.3.tar.bz2
+sudo rpmbuild -ta /scratch/slurm-18.08.3.tar.bz2
 mkdir /scratch/slurm-rpms
 #copy everything from rpmbuild to the shared folder
 sudo cp /root/rpmbuild/RPMS/x86_64/* /scratch/slurm-rpms/
