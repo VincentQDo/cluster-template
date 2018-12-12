c319c4ddcca8a90f5d7c920a593cd5d14a394ca6
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 22:41:19 -0500

    Create nfs_compute.sh

---
+++ nfs_compute.sh
@@ -0,0 +1,6 @@
+sudo mkdir /software
+sudo mount -t nfs 192.168.1.1:/software /software
+sudo mkdir /scratch
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+sudo chmod 755 /local/repository/default_path.sh
+sudo /local/repository/default_path.sh
