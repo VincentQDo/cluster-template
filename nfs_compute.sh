1540e86bbebacf0042b861acdecad4576dcfa1de
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-12 10:27:18 -0500

    Update nfs_compute.sh

--- nfs_compute.sh
+++ nfs_compute.sh
@@ -1,15 +1,15 @@
+sudo mkdir /scratch
+while [ ! -f /scratch/storage.done ]
+do
+  sleep 5
+  sudo mount -t nfs 192.168.1.3:/scratch /scratch
+done
+
 sudo mkdir /software
 while [ ! -f /software/head.done ]
 do
   sleep 5
   sudo mount -t nfs 192.168.1.1:/software /software
-done
-
-sudo mkdir /scratch
-while [ ! -f /software/head.done ]
-do
-  sleep 5
-  sudo mount -t nfs 192.168.1.3:/scratch /scratch
 done
 
 sudo chmod 755 /local/repository/default_path.sh
