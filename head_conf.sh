74732ce19ce265572d067c87c358185cc1498b7c
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 21:05:17 -0500

    Delete head_conf.sh

--- head_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo cp /scratch/slurm.conf /etc/slurm/slurm.conf
-sudo mkdir /var/spool/slurmctld
-sudo chown slurm: /var/spool/slurmctld
-sudo chmod 755 /var/spool/slurmctld
-sudo touch /var/log/slurmctld.log
-sudo chown slurm: /var/log/slurmctld.log
-sudo touch /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
-sudo chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
