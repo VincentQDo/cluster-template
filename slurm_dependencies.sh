6bc2b5b928d67f29256d8b4b5fad134f2f4527ac
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 20:46:35 -0500

    Delete slurm_dependencies.sh

--- slurm_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
