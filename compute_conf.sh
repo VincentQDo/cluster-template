a0d79ae2990af173c50696abf8ea2ce075b43834
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 21:05:23 -0500

    Delete compute_conf.sh

--- compute_conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo cp /scratch/slurm.conf /etc/slurm/slurm.conf
-sudo mkdir /var/spool/slurmd
-sudo chown slurm: /var/spool/slurmd
-sudo chmod 755 /var/spool/slurmd
-sudo touch /var/log/slurmd.log
-sudo chown slurm: /var/log/slurmd.log
