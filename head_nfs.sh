7070b4ef67b426d88e91c9633fc35418b029abf2
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 22:39:04 -0500

    Create head_nfs.sh

---
+++ head_nfs.sh
@@ -0,0 +1,12 @@
+sudo systemctl enable nfs-server.service"))
+sudo systemctl start nfs-server.service"))
+sudo mkdir /software
+sudo chmod -R 777 /software
+sudo chown nfsnobody:nfsnobody /software
+sudo mv /local/repository/xport_software /etc/exports
+sudo chmod 777 /etc/exports
+sudo exportfs -a
+sudo mkdir /scratch
+sudo chmod -R 777 /scratch
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+sudo cp /local/repository/source/* /scratch
