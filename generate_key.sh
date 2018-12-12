a820ecde03aa9dba1e8df1502ffa30be6f89bd73
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 20:40:25 -0500

    Update and rename generate_key.sh to key_onhead.sh

--- generate_key.sh
+++
@@ -1,8 +0,0 @@
-#generate a munge key
-/usr/sbin/create-munge-key -f
-#make the group own the key file
-chown -R munge:munge /etc/munge/munge.key
-#change the permission to 400
-sudo chmod 400 /etc/munge/munge.key
-#copy the key file to scratch to allow other compute nodes to see it and copy it later
-sudo cp /etc/munge/munge.key /scratch/
---
+++ key_onhead.sh
@@ -0,0 +1,37 @@
+#generate a munge key
+/usr/sbin/create-munge-key -f
+#make the group own the key file
+chown -R munge:munge /etc/munge/munge.key
+#change the permission to 400
+sudo chmod 400 /etc/munge/munge.key
+#copy the key file to scratch to allow other compute nodes to see it and copy it later
+sudo cp /etc/munge/munge.key /scratch/
+
+sudo chown -R munge: /etc/munge/ /var/log/munge/
+sudo chmod 0700 /etc/munge/ /var/log/munge/
+
+sudo systemctl enable munge
+sudo systemctl start munge
+
+cd /scratch/
+#link from the tutorial from slothparadise is dead so this is a new link
+wget https://download.schedmd.com/slurm/slurm-18.08.3.tar.bz2
+
+sudo yum install perl-ExtUtils-MakeMaker -y
+sudo rpmbuild -ta /scratch/slurm-18.08.3.tar.bz2
+
+mkdir /scratch/slurm-rpms
+sudo cp /root/rpmbuild/RPMS/x86_64/* /scratch/slurm-rpms/
+#create a flag to let others know that rpm is done building
+sudo touch /scratch/rpm.done
+#install everything in the slurm-rpms folder
+sudo yum --nogpgcheck localinstall /software/slurm-rpms/* -y
+
+sudo cp /scratch/slurm.conf /etc/slurm/slurm.conf
+sudo mkdir /var/spool/slurmctld
+sudo chown slurm: /var/spool/slurmctld
+sudo chmod 755 /var/spool/slurmctld
+sudo touch /var/log/slurmctld.log
+sudo chown slurm: /var/log/slurmctld.log
+sudo touch /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
+sudo chown slurm: /var/log/slurm_jobacct.log /var/log/slurm_jobcomp.log
