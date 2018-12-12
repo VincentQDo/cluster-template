d8f6cc184cdba869c3c38869e8900ef99645ed93
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-11 22:40:35 -0500

    Create storage_nfs.sh

---
+++ storage_nfs.sh
@@ -0,0 +1,8 @@
+sudo systemctl enable nfs-server.service
+sudo systemctl start nfs-server.service
+sudo mkdir /scratch
+sudo chmod -R 777 /scratch
+sudo chown nfsnobody:nfsnobody /scratch
+sudo mv /local/repository/xport_scratch /etc/exports
+sudo chmod 777 /etc/exports
+sudo exportfs -a
