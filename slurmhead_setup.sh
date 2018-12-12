f3b5e8a21888e241182d9344c15bf02187f6de0a
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-12 11:43:27 -0500

    Update slurmhead_setup.sh

--- slurmhead_setup.sh
+++ slurmhead_setup.sh
@@ -1,7 +1,7 @@
 #generate a munge key
 /usr/sbin/create-munge-key -f
 #make the group own the key file
-chown -R munge:munge /etc/munge/munge.key
+sudo chown -R munge:munge /etc/munge/munge.key
 #change the permission to 400
 sudo chmod 400 /etc/munge/munge.key
 #copy the key file to scratch to allow other compute nodes to see it and copy it later
