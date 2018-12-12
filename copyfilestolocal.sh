43ed73b30b58c41414daa5aa1da412cb20091294
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-12 00:53:18 -0500

    Create copyfilestolocal.sh

---
+++ copyfilestolocal.sh
@@ -0,0 +1,5 @@
+sudo cp /local/repository/source/* /scratch
+sudo cp /local/repository/slurm.conf /scratch/
+sudo cp /local/repository/slurmdbd.conf /scratch/
+sudo cp /local/repository/hellobatch.sh /scratch/
+sudo cp /local/repository/pibatch.sh /scratch/
