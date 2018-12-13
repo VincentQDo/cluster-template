b64a51ba65ef725a023a8297cd67720497ef65c6
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-13 11:14:29 -0500

    Update and rename pibatch.sh to pimc_run.sh

---
+++ pimc_run.sh
@@ -0,0 +1,7 @@
+mpicc pi_mc.c -lm
+sbatch -N 2 /local/repository/batch_scripts/2.sh
+sbatch -N 4 /local/repository/batch_scripts/4.sh
+sbatch -N 6 /local/repository/batch_scripts/6.sh
+sbatch -N 8 /local/repository/batch_scripts/8.sh
+sbatch -N 10 /local/repository/batch_scripts/10.sh
+sbatch -N 12 /local/repository/batch_scripts/12.sh
