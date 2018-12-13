4ab17179aece9ebd3bd2966c8f668b673cc030a1
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-12 23:10:31 -0500

    Update 12.sh

--- batch_scripts/12.sh
+++ batch_scripts/12.sh
@@ -1,5 +1,4 @@
 #!/bin/bash  
 set -x  
-cd /scratch/piresults  
 echo "12 nodes"
-time mpirun --allow-run-as-root --np 12 --mca btl_base_warn_component_unused 0 /scratch/a.out 10000000 
+time mpirun --np 12 /scratch/a.out 10000000 
