db932885ad999be434b77487d6dc1a312346033c
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-13 15:28:31 -0500

    Update 12.sh

--- batch_scripts/12.sh
+++ batch_scripts/12.sh
@@ -1,4 +1,10 @@
 #!/bin/bash  
+#SBATCH -N 12
 set -x  
 echo "12 nodes"
+time mpirun --np 2 /scratch/a.out 10000000 
+time mpirun --np 4 /scratch/a.out 10000000 
+time mpirun --np 6 /scratch/a.out 10000000 
+time mpirun --np 8 /scratch/a.out 10000000 
+time mpirun --np 10 /scratch/a.out 10000000 
 time mpirun --np 12 /scratch/a.out 10000000 
