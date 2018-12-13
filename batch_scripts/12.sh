9156ec5dc43d259719c061d10930ae25d4f4a824
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-13 15:41:39 -0500

    Update 12.sh

--- batch_scripts/12.sh
+++ batch_scripts/12.sh
@@ -1,21 +1,5 @@
 #!/bin/bash  
 set -x  
-
-#SBATCH -N 2
-echo "2 nodes" 
-time mpirun /scratch/a.out 10000000 
-#SBATCH -N 4
-echo "4 nodes" 
-time mpirun /scratch/a.out 10000000 
-#SBATCH -N 6
-echo "6 nodes" 
-time mpirun /scratch/a.out 10000000 
-#SBATCH -N 8
-echo "8 nodes" 
-time mpirun /scratch/a.out 10000000 
-#SBATCH -N 10
-echo "10 nodes" 
-time mpirun /scratch/a.out 10000000 
 #SBATCH -N 12
 echo "12 nodes" 
 time mpirun /scratch/a.out 10000000 
