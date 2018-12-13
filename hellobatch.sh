651ef793956678c4838386c6919da35f67be376e
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2018-12-12 23:05:04 -0500

    Delete hellobatch.sh

--- hellobatch.sh
+++
@@ -1,5 +0,0 @@
-mpicc /scratch/hello.c
-mkdir helloresults
-sbatch -N 1 /local/repository/batch_scripts/hi1.sh
-sbatch -N 2 /local/repository/batch_scripts/hi2.sh
-sbatch -N 3 /local/repository/batch_scripts/hi3.sh
